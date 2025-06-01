92f273d815026285a0469d7e343d441841019b43
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2025-06-01 18:57:15 -0400

    reunconfigure cargo

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,7 +8,7 @@ fn temp(s: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![temp])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
