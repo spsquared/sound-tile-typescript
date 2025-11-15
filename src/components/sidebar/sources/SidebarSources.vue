c4a1ea1bb1fe08e531ef4eb4b907791042490112
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2025-11-15 00:11:53 -0800

    add source list & preview in sidebar

--- src/components/sidebar/sources/SidebarSources.vue
+++ src/components/sidebar/sources/SidebarSources.vue
@@ -1,15 +1,142 @@
 <script setup lang="ts">
+import { computed, onUnmounted, reactive, ref, useTemplateRef, watch } from 'vue';
+import { debouncedWatch, throttledWatch } from '@vueuse/core';
+import TileEditor from '@/visualizer/editor';
+import MediaPlayer from '@/visualizer/mediaPlayer';
+import { Tile, VisualizerTile } from '@/visualizer/tiles';
 import SidebarContentWrapper from '../SidebarContentWrapper.vue';
+import SourceItem from './SourceItem.vue';
 
+const sidebarVisible = computed(() => TileEditor.state.sidebarOpen && TileEditor.state.sidebarTab == 'sources');
+
+// update UI only when visible and when tiles settled
+const debouncedTiles = ref<Set<Tile>>(new Set());
+debouncedWatch([
+    sidebarVisible,
+    TileEditor.currentTiles
+], () => {
+    if (sidebarVisible.value) debouncedTiles.value = TileEditor.currentTiles.value;
+}, { debounce: 100, deep: false });
+
+// sources mapped to tile sets
+const sources = computed(() => {
+    const collectedSources: Map<ArrayBuffer, Set<VisualizerTile>> = new Map();
+    for (const tile of debouncedTiles.value) {
+        if (tile instanceof VisualizerTile && tile.visualizer.data.buffer !== null) {
+            const buffer = tile.visualizer.data.buffer;
+            if (!collectedSources.has(buffer)) collectedSources.set(buffer, new Set());
+            collectedSources.get(buffer)!.add(tile);
+        }
+    }
+    return [...collectedSources.entries()].map<{
+        buffer: ArrayBuffer
+        tiles: Set<VisualizerTile>
+        key: string
+    }>(([buffer, tiles]) => {
+        // hopefully consistent keys
+        const key = tiles.values().next().value?.id.toString() ?? 'uh oh empty';
+        return {
+            buffer: buffer,
+            tiles: tiles,
+            key: key
+        };
+    });
+});
+
+// the source preview gets its own audio context
+const audioContext = new AudioContext();
+const gain = audioContext.createGain();
+gain.connect(audioContext.destination);
+watch(() => MediaPlayer.state.volume, () => gain.gain.value = MediaPlayer.state.volume, { immediate: true });
+onUnmounted(() => audioContext.close());
+
+// playback handled here, source items just make the UI
+const playbackNodes: Map<string, {
+    buffer: AudioBuffer,
+    node: AudioBufferSourceNode | null
+}> = reactive(new Map());
+throttledWatch(sources, async () => {
+    // diff system, avoids decoding all audio on every tiny change (REALLY BAD!!!)
+    const newKeys = new Set<String>();
+    for (const { buffer, key } of sources.value) {
+        newKeys.add(key);
+        if (!playbackNodes.has(key)) audioContext.decodeAudioData(buffer.slice()).then((buffer) => {
+            playbackNodes.set(key, {
+                buffer: buffer,
+                node: null
+            });
+        });
+    }
+    for (const [key, node] of playbackNodes) {
+        if (!newKeys.has(key)) {
+            node.node?.stop();
+            node.node?.disconnect();
+            playbackNodes.delete(key);
+        }
+    }
+}, { throttle: 500, leading: true, trailing: true });
+
+const sourceItems = useTemplateRef('sourceItems');
+function playPreview(sourceKey: string, t: number) {
+    const node = playbackNodes.get(sourceKey);
+    if (node === undefined) return;
+    if (sourceItems.value !== null) for (const source of sourceItems.value) {
+        if (source?.$props.sourceKey !== sourceKey) source?.forcePause(); // me when I add entire prop for this
+    }
+    for (const node of playbackNodes.values()) {
+        node.node?.stop();
+        node.node?.disconnect();
+    }
+    node.node = audioContext.createBufferSource();
+    node.node.buffer = node.buffer;
+    node.node.connect(gain);
+    node.node.start(audioContext.currentTime, t);
+}
+function pausePreview(sourceKey: string) {
+    const node = playbackNodes.get(sourceKey);
+    if (node === undefined) return;
+    node.node?.stop();
+    node.node?.disconnect();
+}
+
+// console.log('stop')
+// const node = playbackNodes.get(source.$props.sourceKey);
+// if (node === undefined) return;
+// node.node?.stop();
+// node.node?.disconnect();
+
+watch(sidebarVisible, () => {
+    if (!sidebarVisible.value) {
+        if (sourceItems.value !== null) for (const source of sourceItems.value) {
+            source?.forcePause();
+        }
+        for (const node of playbackNodes.values()) {
+            node.node?.stop();
+            node.node?.disconnect();
+            node.node = null;
+        }
+        audioContext.suspend();
+    } else audioContext.resume();
+});
 </script>
 
 <template>
     <SidebarContentWrapper tab="sources">
         <template v-slot:header>Sources</template>
         <template v-slot:content>
-            wow look at all those sources
+            <SourceItem v-for="source in sources" :key="source.key" :source-key="source.key" ref="sourceItems" :tiles="source.tiles" :preview-duration="playbackNodes.get(source.key)?.buffer.duration" :play-preview="(t) => playPreview(source.key, t)" :pause-preview="() => pausePreview(source.key)"></SourceItem>
+            <div class="noSources" v-if="sources.length == 0">
+                No sources added!
+                <br>
+                Yes there will be a tutorial button here at some point
+            </div>
         </template>
     </SidebarContentWrapper>
 </template>
 
-<style scoped></style>
+<style scoped>
+.noSources {
+    margin-top: 1em;
+    text-align: center;
+}
+</style>
---
+++ src/components/sidebar/sources/SourceItem.vue
@@ -0,0 +1,194 @@
+<script setup lang="ts">
+import { computed, onBeforeUnmount, ref, watch } from 'vue';
+import TileEditor from '@/visualizer/editor';
+import MediaPlayer from '@/visualizer/mediaPlayer';
+import { VisualizerTile } from '@/visualizer/tiles';
+import Slider from '@/components/inputs/Slider.vue';
+
+const props = defineProps<{
+    sourceKey: string // literally useless here, only used in parent to not pause this when playing
+    playPreview: (t: number) => void
+    pausePreview: () => void
+    tiles: Set<VisualizerTile>
+    previewDuration: number | undefined
+}>();
+
+// wow this is a lot more complicated than I remember it being... and yes it has to be this way
+const playing = ref(false);
+const startTime = ref(0);
+const currentTime = ref(0);
+const timeStr = computed(() => `${MediaPlayer.formatTime(currentTime.value)} / ${MediaPlayer.formatTime(props.previewDuration ?? 0)}`);
+defineExpose({
+    forcePause: () => playing.value = false
+});
+const seekTime = computed<number>({
+    get: () => currentTime.value,
+    set: (t) => {
+        startTime.value = performance.now() / 1000 - t;
+        currentTime.value = t;
+        if (playing.value) props.playPreview(currentTime.value);
+    }
+});
+watch(playing, () => {
+    if (playing.value) props.playPreview(currentTime.value);
+    else props.pausePreview();
+});
+let refreshInterval: NodeJS.Timeout = setInterval(() => {
+    const now = performance.now() / 1000;
+    if (playing.value) {
+        currentTime.value = now - startTime.value;
+        if (currentTime.value >= (props.previewDuration ?? 0)) seekTime.value = 0;
+    } else {
+        startTime.value = now - currentTime.value;
+    }
+}, 50);
+onBeforeUnmount(() => clearInterval(refreshInterval));
+
+const idThing = 'previewPlay' + performance.now() + Math.random();
+
+function setIdentifyTile(tile: VisualizerTile, v: boolean) {
+    if (v) TileEditor.state.identifyTilesSidebar.add(tile);
+    else TileEditor.state.identifyTilesSidebar.delete(tile);
+}
+</script>
+
+<template>
+    <div class="sourceItem">
+        <div class="preview">
+            <input type="checkbox" :id="idThing" class="previewPlayCheckbox" v-model="playing"></input>
+            <label button class="previewPlayButton" :for="idThing" v-if="props.previewDuration !== undefined" :title="playing ? 'Pause preview' : 'Play preview'"></label>
+            <div class="previewLoadingStandin" v-else></div>
+            <Slider class="previewSeekSlider" v-model="seekTime" :title="timeStr" :min="0" :max="props.previewDuration ?? 0" :step="0.01" length="100%" track-width="40px" thumb-width="40px" thumb-length="16px" thumb-radius="0px" color2="#555" color3="#DDD" color4="#EEE" side-border-width="0px" end-border-width="0px"></Slider>
+            <div class="previewTimeDisplay" :title="timeStr">{{ MediaPlayer.formatTime(currentTime) }}</div>
+        </div>
+        <template v-for="tile in props.tiles">
+            <div class="tileLink" @mouseenter="setIdentifyTile(tile, true)" @mouseleave="setIdentifyTile(tile, false)">{{ tile.label }}</div>
+            <div class="tileLinkDivider"></div>
+        </template>
+    </div>
+</template>
+
+<style scoped>
+.sourceItem {
+    display: flex;
+    flex-direction: column;
+    row-gap: 2px;
+    margin-bottom: 8px;
+    padding: 8px 8px;
+    background-color: #222;
+}
+
+.sourceItem:hover {
+    background-color: #333;
+}
+
+.preview {
+    display: grid;
+    grid-template-columns: 40px 1fr 60px;
+    column-gap: 2px;
+    border: 2px solid white;
+    margin-bottom: 6px;
+    background-color: white;
+}
+
+.previewPlayButton {
+    grid-row: 1;
+    grid-column: 1;
+    border-radius: 0px;
+    background-color: #A00;
+    background-image: url(@/img/play.svg);
+    background-position: center;
+    background-size: 50% 50%;
+    background-repeat: no-repeat;
+}
+
+.previewPlayButton:hover {
+    background-color: #C00;
+}
+
+.previewPlayCheckbox {
+    display: none;
+}
+
+.previewPlayCheckbox:checked+.previewPlayButton {
+    background-color: #080;
+    background-image: url(@/img/pause.svg);
+}
+
+.previewPlayCheckbox:checked+.previewPlayButton:hover {
+    background-color: #0A0;
+}
+
+.previewLoadingStandin {
+    grid-row: 1;
+    grid-column: 1;
+    position: relative;
+    background-color: #555;
+    cursor: not-allowed;
+}
+
+.previewLoadingStandin::before,
+.previewLoadingStandin::after {
+    content: ' ';
+    display: block;
+    position: absolute;
+    top: 4px;
+    left: 4px;
+    width: 16px;
+    height: 16px;
+    animation: previewLoadingSpinner 1000ms cubic-bezier(0.7, 0, 0.8, 0.5) infinite;
+    background-color: white;
+    box-shadow: 0px 0px 2px 0px #FFFA;
+}
+
+.previewLoadingStandin::after {
+    animation-delay: -625ms;
+}
+
+@keyframes previewLoadingSpinner {
+    0% {
+        transform: translate(0px, 0px);
+    }
+
+    25% {
+        transform: translate(16px, 0px);
+    }
+
+    50% {
+        transform: translate(16px, 16px);
+    }
+
+    75% {
+        transform: translate(0px, 16px);
+    }
+}
+
+.previewSeekSlider {
+    grid-row: 1;
+    grid-column: 2;
+    max-width: 100%;
+}
+
+.previewTimeDisplay {
+    grid-row: 1;
+    grid-column: 3;
+    align-content: center;
+    text-align: center;
+    background-color: black;
+}
+
+.tileLink {
+    border-radius: 8px;
+    padding: 0px 24px;
+}
+
+.tileLink:hover {
+    background-color: #555;
+}
+
+.tileLinkDivider {
+    height: 2px;
+    margin: 0px 16px;
+    background-color: #555;
+}
+</style>
